595722e82c91cc77a922a52ba1dfbd11f4a1f6c1
GenyaSochi <eunasedkina@gmail.com>
2024-08-11 22:57:00 +0300

    _

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
-<h1 v-if="bool" @click="str+='!'">{{ str }}</h1>
-<h1 v-else @click="str+='!'">{{ str.split('').reverse().join('') }}</h1>
-<button v-on:click= "myFunc":style="'color:#' + color">Reverse</button>
+  <h1 v-if="bool" @click="str += '!'">{{ str }}</h1>
+  <h1 v-else @click="str += '!'">{{ str.split('').reverse().join('') }}</h1>
+  <button v-on:click="myFunc" :style="'color:' + color">Reverse</button>
+  <p></p>
+  <input v-model="message" />
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
+
+
 const str = ref('Learn vue')
 const bool = ref(true)
-const color = ref(100000)
-color.value = 100000
-const myFunc = ()=>{
-  bool.value =! bool.value
-  color.value +=500
-}
+const color = ref('red')
+const message = ref('')
 
+
+const myFunc = () => {
+  bool.value = !bool.value
+  color.value += 500
+  console.log(message.value)
+  message.value = '' 
+}
 </script>
 
 <style scoped>
-h1{
+h1 {
   user-select: none;
 }
+
+
 </style>
