1ee53294e758142daac0a27a72d9a2442e8d54a3
GenyaSochi <eunasedkina@gmail.com>
2024-08-11 13:47:28 +0300

    _

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <h1>{{ msg }}{{ 1+1 }}</h1>
 
   <div class="card">
-    <button type="button" :style="'color:#'+ color" @click="count++">count is {{ count }}</button>
+    <button type="button" :style="'color:#'+ color" @click="myClickFunc">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -28,11 +28,12 @@ defineProps<{ msg: string }>()
 
 const color = ref(555555)
 const count = ref(0)
-// function myClick(){
-//   count.value++
-//   color.value+=500
-// }
+function myClickFunc(){
+  count.value++
+  color.value+=500
+}
 </script>
+
 <style scoped>
 .read-the-docs {
   color: #888;
