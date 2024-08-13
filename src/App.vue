a759321ddabccc01c55b5445dfcfb9339d9a7cad
Genyasochi <eunasedkina@gmail.com>
2024-08-13 21:12:56 +0300

    _

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,10 @@
 <template>
-  <form @submit.prevent="add">
-    <input v-model="message" placeholder="todo" />
-  </form>
-  <ul>
-    <li :data-check="el.check" v-for="el of arr" :key="el.id">{{ el.text }} 
-      <span v-if="el.check == 0">
-        <button @click="check(el.id,1)">✔</button>
-        <button @click="check(el.id,2)">🤢</button>
-      </span>
-    </li>
-  </ul>
+<ToDoList></ToDoList>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const message = ref('')
-
-const arr = ref((JSON.parse(localStorage.arr) || []) as any[]) 
-
-const add = () => {
-  arr.value.push(
-    { text: message.value, id: arr.value.length, check: 0 },
-  )
-  localStorage.arr = JSON.stringify(arr.value)
-  message.value = ''
-}
-
-const check = (id:number, check:number) => {
-  const el = arr.value.find(el=>el.id==id)
-  el.check = check
-  localStorage.arr = JSON.stringify(arr.value)
-}
-
+import ToDoList from './components/ToDoList.vue'
 </script>
 
 <style scoped>
-[data-check="1"] {
-  text-decoration: line-through;
-}
-[data-check="2"] {
-  background-color: red;
-}
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="add">
+    <input v-model="message" placeholder="todo" />
+  </form>
+  <ul>
+    <li :data-check="el.check" v-for="el of arr" :key="el.id">{{ el.text }} 
+      <span v-if="el.check == 0">
+        <button @click="check(el.id,1)">✔</button>
+        <button @click="check(el.id,2)">🤢</button>
+      </span>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('')
+
+const arr = ref((JSON.parse(localStorage.arr) || []) as any[]) 
+
+const add = () => {
+  arr.value.push(
+    { text: message.value, id: arr.value.length, check: 0 },
+  )
+  localStorage.arr = JSON.stringify(arr.value)
+  message.value = ''
+}
+
+const check = (id:number, check:number) => {
+  const el = arr.value.find(el=>el.id==id)
+  el.check = check
+  localStorage.arr = JSON.stringify(arr.value)
+}
+
+</script>
+
+<style scoped>
+[data-check="1"] {
+  text-decoration: line-through;
+}
+[data-check="2"] {
+  background-color: red;
+}
+</style>
