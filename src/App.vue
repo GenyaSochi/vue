46356aec3a997b42fd99f4c3e863405d4c0f97c2
Genyasochi <eunasedkina@gmail.com>
2024-08-15 21:11:14 +0300

    _

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
 <ToDoList></ToDoList>
+<UserInput></UserInput>
+
 </template>
 
 <script setup lang="ts">
 import ToDoList from './components/ToDoList.vue'
+import UserInput from './components/UserInput.vue'
+console.log('sdfsd')
 </script>
 
-<style scoped>
-</style>
---
+++ src/components/UserInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input @input="changeName" v-model="name" placeholder="Введите имя пользователя" />
+  {{ name }}
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+const name = ref('')
+const changeName = (e:any) => {
+  const target = e.target as HTMLInputElement
+  const notNeed = '01234546789+-*|'
+  let name = ''
+  for (let el of target.value) {
+    if (notNeed.includes(el)) continue
+    name+=el
+  }
+  target.value = name
+}
+
+</script>
+
+<style>
+
+</style>
