b2bf7156b31cfc921d2a673cb446d3f927363004
Genyasochi <eunasedkina@gmail.com>
2024-08-22 21:42:31 +0300

    _

--- src/components/StudentGroup.vue
+++ src/components/StudentGroup.vue
@@ -1,22 +1,24 @@
 <template>
 <div>
   <select name="Group" id="1">
-    <option value=""></option>
-    <option value=""></option>
-    <option value=""></option>
+    <option value="Group1">Группа 1</option>
+    <option value="Group2">Группа 2</option>
+    <option value="Group3">Группа 3</option>
   </select>
   <select name="Lesson" id="2">
-    <option value=""></option>
-    <option value=""></option>
-    <option value=""></option>
+    <option value="Russian language">Русский язык</option>
+    <option value="Informatics">Информатика</option>
+    <option value="Mathematics">Математика</option>
   </select>
-  <button></button>
+  <button ></button>
 </div>
 </template>
 
 
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+const a = 1
+
 </script>
 
 <style>
