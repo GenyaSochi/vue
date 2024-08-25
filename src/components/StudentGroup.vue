2b24e7de6449e589c6326d4cb9f8d793254d6de5
GenyaSochi <eunasedkina@gmail.com>
2024-08-25 12:16:16 +0300

    _

--- src/components/StudentGroup.vue
+++ src/components/StudentGroup.vue
@@ -1,28 +1,47 @@
 <template>
   <div class="journal">
-    <div>Group:</div>
-    <select style="width: 110px; height: 24px;" name="Group" id="1" v-model="select">
-      <option value="Group1">Group 1</option>
-      <option value="Group2">Group 2</option>
-      <option value="Group3">Group 3</option>
-    </select>
-    <div>Lesson:</div>
-    <select style="width: 110px; height: 24px;" name="Lesson" id="2" v-model="select1">
-      <option value="Lesson1">1</option>
-      <option value="Lesson2">2</option>
-      <option value="Lesson3">3</option>
-      <option value="Lesson4">4</option>
-      <option value="Lesson5">5</option>
-      <option value="Lesson6">6</option>
-    </select>
-    <button style="background-color: #e1e1e1; width: 100px; height: 42px;">Select</button>
-  </div>
-  <hr>
-  <div class="journal">
-    <div>Topic:</div>
-<input style="width: 400px;" type="text" v-model="message" placeholder="Lesson topic">
-  </div>
-  <div><span>Name</span><span>Is present</span>
+    <div class="journal1">
+      <div>Group:</div>
+      <select style="width: 110px; height: 24px;" name="Group" id="1" v-model="select">
+        <option value="Group1">Group 1</option>
+        <option value="Group2">Group 2</option>
+        <option value="Group3">Group 3</option>
+      </select>
+      <div class="journal2">Lesson:</div>
+      <select style="width: 110px; height: 24px;" name="Lesson" id="2" v-model="select1">
+        <option value="Lesson1">1</option>
+        <option value="Lesson2">2</option>
+        <option value="Lesson3">3</option>
+        <option value="Lesson4">4</option>
+        <option value="Lesson5">5</option>
+        <option value="Lesson6">6</option>
+      </select>
+      <button style="background-color: #e1e1e1; width: 100px; height: 42px;">Select</button>
+    </div>
+    <hr>
+    <div class="journal1">
+      <div>Topic:</div>
+      <input style="width: 432px;" type="text" v-model="message" placeholder="Lesson topic">
+    </div>
+    <div><span>Name</span><span>Is present</span>
+    </div>
+    <hr>
+    <div>Student 1</div>
+    <label>
+      <input type="checkbox" v-model="checkbox1">
+    </label>
+    <hr>
+    <div>Student 2</div>
+    <label>
+      <input type="checkbox" v-model="checkbox2">
+    </label>
+    <hr>
+    <div>Student 3</div>
+    <label>
+      <input type="checkbox" v-model="checkbox3">
+    </label>
+    <hr>
+    <button style="background-color: #e1e1e1; width: 500px; height: 42px;">Save</button>
   </div>
 </template>
 
@@ -33,18 +52,33 @@ import { computed, ref } from 'vue'
 const message = ref('JS.Document object model')
 const select = ref('')
 const select1 = ref('')
+const checkbox1 = ref('')
+const checkbox2 = ref('')
+const checkbox3 = ref('')
 </script>
 
 <style scoped>
 .journal {
+  display: contents;
+  width: 600px;
+  height: 400px;
+}
+
+.journal1 {
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 20px;
 }
-hr {
-  width: 470px;
+
+.journal2 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
 }
 
-
+hr {
+  width: 500px;
+}
 </style>
