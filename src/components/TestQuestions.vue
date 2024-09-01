878217b16426deefad5cfacdb02fb2b768ab98ed
GenyaSochi <eunasedkina@gmail.com>
2024-09-01 23:31:19 +0300

    _

--- src/components/TestQuestions.vue
+++ src/components/TestQuestions.vue
@@ -1,39 +1,54 @@
 <template>
   <h4>ТЕСТ</h4>
-  <div class="test">
-    <div v-for="question in questions">Вопрос: {{ question.question }}
-    <div>Выберите один вариант ответа:  {{ question.answers }} 
-      <input type="radio"> 
-      <input type="radio">
-      <input type="radio">
-    </div>
-
-    <button>Добавить ответ</button>
+  <div style="margin-bottom: 10px;" v-for="variant in variants">{{ variant.question }}
+    <div style="margin-bottom: 10px;">Варианты ответов:</div>
+    <p><input type="radio" name="radio">{{ variant.answer1 }}</p>
+    <br>
+    <p><input type="radio">{{ variant.answer2 }}</p>
   </div>
+  <br>
 
-
+  <div>
+    <button style="padding: 10px;" @click="addCounter">Ответить</button>
   </div>
+  <div>Количество правильных ответов: {{ counter }}</div>
+
 
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 
-const questions = [
-  { question: 'Сколько месяцев в году?', answers: [12 , 13, 3], rightAnswer: 12 },
-  { question: 'Сколько сантиметров в метре?', answers: [100, 35, 200], rightAnswer: 100 },
+const counter = ref(0)
+
+
+
+const variants = [
+  { id: 1, question: 'Сколько сантиметров метре?', answer1: 100, answer2: 200, correctAnswer: 100 },
+  { id: 2, question: 'Сколько месяцев в году?', answer1: 13, answer2: 12, correctAnswer: 12 }
 ]
+const addCounter = () => {
+  counter.value++
+}
 
+let answer = (answers: any[]) => {
+  answers.join(', ')
+}
 
-
+const correctAnswer = () => {
+  if (answer = correctAnswer)
+    counter.value += 1
+}
 </script>
 
+
 <style scoped>
-.test {
+* {
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
 </style>
