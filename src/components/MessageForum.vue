cf7ebeabacd3d99b54b075ba5faf89bda2366e87
Genyasochi <eunasedkina@gmail.com>
2024-09-05 21:13:53 +0300

    _

--- src/components/MessageForum.vue
+++ src/components/MessageForum.vue
@@ -1,9 +1,9 @@
 <template>
   <form class="form" @submit.prevent="">
     <p class="name"><span>Name</span><span>{{ date }}</span></p>
-    <p class="input">Youe name: <input type="text" :value="yourName" @input="inputYourName" placeholder="Имя"
+    <p class="input">Youe name: <input type="text" v-model="name" placeholder="Имя"
         style="width: 696px;"></p>
-    <p class="input">Youe message: <input type="text" :value="yuorMessage" @input="inputYourMessage"
+    <p class="input">Youe message: <input type="text"  v-model="message"
         placeholder="Сообщение" style="width: 674px;"></p>
     <button @click.prevent="addSave" style="width: 100%; background-color: darkgray;">Save</button>
   </form>
@@ -13,28 +13,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const yourName = ref('')
-const yuorMessage = ref('')
-const newMessage = localStorage
-const newName = ref('')
-const date = new Date()
-date.getFullYear()
+const name = ref('')
+const message = ref('')
+
+type chat = {
+  name:string,
+  message:string,
+}
+
+const newMessage = localStorage.arr ? JSON.parse(localStorage.arr) : [] as chat[]
+const arr = ref(newMessage)
+
+const date = new Date().toLocaleString()
+
 
 
 
 const addSave = () => {
-  if (yourName.value != '' && yuorMessage.value != '')
-    return newMessage
+  if 
+  
 }
 
-const inputYourName = (event: { target: { value: string; }; }) => {
-  yourName.value = event.target.value
 
-}
-
-const inputYourMessage = (event: { target: { value: string }; }) => {
-  yuorMessage.value = event.target.value
-}
 
 </script>
 
