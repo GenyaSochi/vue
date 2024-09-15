6d7eb87ecdb712ca870e96410650590afbad4ef0
GenyaSochi <eunasedkina@gmail.com>
2024-09-15 20:42:40 +0300

    _

--- src/components/TextForm.vue
+++ src/components/TextForm.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="box">
-    <input type="checkbox" value="bold" name="fontStyle" v-model="message"><span>Bold</span>
-    <input type="checkbox" value="underline" name="fontStyle" v-model="message"><span>Underline</span>
-    <input type="checkbox" value="italics" name="fontStyle" v-model="message"><span>Italics</span>
-    <input type="radio" value="left" name="fontStyle" v-model="message"><span>Left</span>
-    <input type="radio" value="right" name="fontStyle" v-model="message"><span>Right</span>
-    <input type="radio" value="jastify" name="fontStyle" v-model="message"><span>Jastify</span>
+    <input type="checkbox" value="bold" name="fontStyle" v-model="bold"><span>Bold</span>
+    <input type="checkbox" value="underline" name="fontStyle" v-model="underline"><span>Underline</span>
+    <input type="checkbox" value="italics" name="fontStyle" v-model="italics"><span>Italics</span>
+    <input type="radio" value="left" name="fontStyle" v-model="left"><span>Left</span>
+    <input type="radio" value="right" name="fontStyle" v-model="right"><span>Right</span>
+    <input type="radio" value="jastify" name="fontStyle" v-model="jastify"><span>Jastify</span>
 
-    <input style="width: 600px; height: 200px;" type="text">
+    <input style="width: 600px; height: 200px;" type="text" v-model="message">
     <div>
       <button class="show">Show text</button>
     </div>
@@ -15,13 +15,29 @@
 </template>
 
 <script setup lang="ts">
-
+import test from 'node:test';
 import { ref } from 'vue'
+
+const bold = ref('')
+const underline = ref('')
+const italics = ref('')
+const left = ref('')
+const right = ref('')
+const jastify = ref('')
 const message = ref('')
+
+const localArr = localStorage.arr ? JSON.parse(localStorage.arr) : []
+const arr = ref(localArr as any[])
+
+
+
+
 
 const addText = ()=>{
   if(message.value != ''){
-    
+    arr.value.push({
+      text: message.value
+    })
   }
 }
 
