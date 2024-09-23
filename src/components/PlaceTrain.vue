4f61eb5ca9099af253a04bc0662f524554e918c4
GenyaSochi <eunasedkina@gmail.com>
2024-09-23 22:42:07 +0300

    _

--- src/components/PlaceTrain.vue
+++ src/components/PlaceTrain.vue
@@ -1,133 +1,90 @@
 <template>
-  <div style="background-color: gainsboro; padding-top: 20px;">
+  <div
+    style="background-color: gainsboro; padding-top: 20px;border: 1px solid gray; margin: 20px 60px; border-radius: 4px;">
     <span style="padding-right: 10px;">Direction:</span>
-    <select style="width: 220px;">
+    <select style="width: 220px;" v-model="directions">
       <option>Москва-Сочи</option>
-      <option>Казань-Новосибирск</option>
-      <option>Иркутск-Екатеринбург</option>
+      <option>Екатеринбург-Новосибирск</option>
+      <option>Казань-Иркутск</option>
     </select>
     <label for="trainDate" style="padding: 0 12px;">Date:</label>
-    <input type="date" id="trainDate" value="newDate" style="margin-right: 40px;">
-    <button style="width: 100px; border: 2px solid black;">Search</button>
+    <input type="date" id="trainDate" value="newDate" v-model="choiceDate" style="margin-right: 40px;">
+    <button @click="addTiket" style="width: 100px; border: 2px solid black;">Search</button>
     <hr style="width: 90%;">
-    <div style="border: 1px solid gray; margin: 20px 60px; border-radius: 4px;padding-top: 20px;">
-      <div>
-        <div>
 
-        </div>
-        <span class="place">
-          <input type="checkbox" id="1" style="width: 20px;">1
-        </span>
-        <span class="place">
-          <input type="checkbox" id="3" style="width: 20px;">3
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="5" style="width: 20px;">5
-        </span>
-        <span class="place">
-          <input type="checkbox" id="7" style="width: 20px;">7
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="9">9
-        </span>
-        <span class="place">
-          <input type="checkbox" id="11" >11
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="13" >13
-        </span>
-        <span class="place">
-          <input type="checkbox" id="15" >15
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="17" >17
-        </span>
-        <span class="place">
-          <input type="checkbox" id="19" >19
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="21">21
-        </span>
-        <span class="place">
-          <input type="checkbox" id="23" >23
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="25" >25
-        </span>
-        <span class="place">
-          <input type="checkbox" id="27" >27
-        </span>
-      </div>
-      <div>
-        <span class="place">
-          <input type="checkbox" id="2" >2
-        </span>
-        <span class="place">
-          <input type="checkbox" id="4">4
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="6">6
-        </span>
-        <span class="place">
-          <input type="checkbox" id="8">8
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="10">10
-        </span>
-        <span class="place">
-          <input type="checkbox" id="12">12
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="14">14
-        </span>
-        <span class="place">
-          <input type="checkbox" id="16">16
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="18">18
-        </span>
-        <span class="place">
-          <input type="checkbox" id="20">20
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="22">22
-        </span>
-        <span class="place">
-          <input type="checkbox" id="24">24
-        </span>
-        <span style="margin: 1px;"></span>
-        <span class="place">
-          <input type="checkbox" id="26">26
-        </span>
-        <span class="place">
-          <input type="checkbox" id="28" style="width: 20px;">28
-        </span>
-
-      </div>
+    <div>
+      <p>
+        <input type="checkbox" id="1" style="width: 20px;">1
+        <input type="checkbox" id="3" style="width: 20px;">3
+        <input type="checkbox" id="5" style="width: 20px;">5
+        <input type="checkbox" id="7" style="width: 20px;">7
+        <input type="checkbox" id="9" style="width: 20px;">9
+        <input type="checkbox" id="11" style="width: 18px;">11
+        <input type="checkbox" id="13" style="width: 18px;">13
+        <input type="checkbox" id="15" style="width: 18px;">15
+        <input type="checkbox" id="17" style="width: 18px;">17
+        <input type="checkbox" id="19" style="width: 18px;">19
+        <input type="checkbox" id="21" style="width: 18px;">21
+        <input type="checkbox" id="23" style="width: 18px;">23
+        <input type="checkbox" id="25" style="width: 18px;">25
+        <input type="checkbox" id="27" style="width: 18px;">27
+      </p>
     </div>
-
-
+    <div>
+      <p>
+        <input type="checkbox" id="2" style="width: 20px;">2
+        <input type="checkbox" id="4" style="width: 20px;">4
+        <input type="checkbox" id="6" style="width: 20px;">6
+        <input type="checkbox" id="8" style="width: 20px;">8
+        <input type="checkbox" id="10" style="width: 18px;">10
+        <input type="checkbox" id="12" style="width: 18px;">12
+        <input type="checkbox" id="14" style="width: 18px;">14
+        <input type="checkbox" id="16" style="width: 18px;">16
+        <input type="checkbox" id="18" style="width: 18px;">18
+        <input type="checkbox" id="20" style="width: 18px;">20
+        <input type="checkbox" id="22" style="width: 18px;">22
+        <input type="checkbox" id="24" style="width: 18px;">24
+        <input type="checkbox" id="26" style="width: 18px;">26
+        <input type="checkbox" id="28" style="width: 18px;">28
+      </p>
+    </div>
   </div>
-
-
 </template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
+
+const directions = ref('')
+const choiceDate = ref('')
+const cost = ref('')
+
+
+
+
+const tikets = localStorage.textArr ? JSON.parse(localStorage.textArr) : [] as BuyTiket[]
+const arr = ref(tikets as BuyTiket[])
+
+type BuyTiket = {
+  direction: string,
+  cost: any,
+  date: any,
+ 
+}
+
+const tiket = ref([] as BuyTiket[])
+
+const addTiket = ()=>{
+ 
+    arr.value.push({
+      cost: cost.value, direction: directions.value, date: choiceDate.value, 
+    })
+    localStorage.textArr = JSON.stringify(arr.value)
+  }
+
 </script>
 <style scoped>
 .place {
   border: 1px solid gray;
- 
+
 }
 </style>
