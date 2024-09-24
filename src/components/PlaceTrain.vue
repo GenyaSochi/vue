a18ad3dc2405ed6adf7d608e02e1e3ef04d2a35b
GenyaSochi <eunasedkina@gmail.com>
2024-09-24 23:55:35 +0300

    _

--- src/components/PlaceTrain.vue
+++ src/components/PlaceTrain.vue
@@ -15,38 +15,38 @@
 
     <div>
       <p>
-        <input type="checkbox" id="1" style="width: 20px;">1
-        <input type="checkbox" id="3" style="width: 20px;">3
-        <input type="checkbox" id="5" style="width: 20px;">5
-        <input type="checkbox" id="7" style="width: 20px;">7
-        <input type="checkbox" id="9" style="width: 20px;">9
-        <input type="checkbox" id="11" style="width: 18px;">11
-        <input type="checkbox" id="13" style="width: 18px;">13
-        <input type="checkbox" id="15" style="width: 18px;">15
-        <input type="checkbox" id="17" style="width: 18px;">17
-        <input type="checkbox" id="19" style="width: 18px;">19
-        <input type="checkbox" id="21" style="width: 18px;">21
-        <input type="checkbox" id="23" style="width: 18px;">23
-        <input type="checkbox" id="25" style="width: 18px;">25
-        <input type="checkbox" id="27" style="width: 18px;">27
+        <input type="checkbox" style="width: 20px;">1
+        <input type="checkbox" style="width: 20px;">3
+        <input type="checkbox" style="width: 20px;">5
+        <input type="checkbox" style="width: 20px;">7
+        <input type="checkbox" style="width: 20px;">9
+        <input type="checkbox" style="width: 18px;">11
+        <input type="checkbox" style="width: 18px;">13
+        <input type="checkbox" style="width: 18px;">15
+        <input type="checkbox" style="width: 18px;">17
+        <input type="checkbox" style="width: 18px;">19
+        <input type="checkbox" style="width: 18px;">21
+        <input type="checkbox" style="width: 18px;">23
+        <input type="checkbox" style="width: 18px;">25
+        <input type="checkbox" style="width: 18px;">27
       </p>
     </div>
     <div>
       <p>
-        <input type="checkbox" id="2" style="width: 20px;">2
-        <input type="checkbox" id="4" style="width: 20px;">4
-        <input type="checkbox" id="6" style="width: 20px;">6
-        <input type="checkbox" id="8" style="width: 20px;">8
-        <input type="checkbox" id="10" style="width: 18px;">10
-        <input type="checkbox" id="12" style="width: 18px;">12
-        <input type="checkbox" id="14" style="width: 18px;">14
-        <input type="checkbox" id="16" style="width: 18px;">16
-        <input type="checkbox" id="18" style="width: 18px;">18
-        <input type="checkbox" id="20" style="width: 18px;">20
-        <input type="checkbox" id="22" style="width: 18px;">22
-        <input type="checkbox" id="24" style="width: 18px;">24
-        <input type="checkbox" id="26" style="width: 18px;">26
-        <input type="checkbox" id="28" style="width: 18px;">28
+        <input type="checkbox" style="width: 20px;">2
+        <input type="checkbox" style="width: 20px;">4
+        <input type="checkbox" style="width: 20px;">6
+        <input type="checkbox" style="width: 20px;">8
+        <input type="checkbox" style="width: 18px;">10
+        <input type="checkbox" style="width: 18px;">12
+        <input type="checkbox" style="width: 18px;">14
+        <input type="checkbox" style="width: 18px;">16
+        <input type="checkbox" style="width: 18px;">18
+        <input type="checkbox" style="width: 18px;">20
+        <input type="checkbox" style="width: 18px;">22
+        <input type="checkbox" style="width: 18px;">24
+        <input type="checkbox" style="width: 18px;">26
+        <input type="checkbox" style="width: 18px;">28
       </p>
     </div>
     <table>
