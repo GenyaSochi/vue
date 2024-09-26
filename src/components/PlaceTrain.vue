164413e035a020a0b3ffc7cf6b1bb55a1b28f516
Genyasochi <eunasedkina@gmail.com>
2024-09-26 21:10:34 +0300

    _

--- src/components/PlaceTrain.vue
+++ src/components/PlaceTrain.vue
@@ -10,43 +10,23 @@
     </select>
     <label for="trainDate" style="padding: 0 12px;">Date:</label>
     <input type="date" v-model="choiceDate" style="margin-right: 40px;">
-    <button @click="addTiket" style="width: 100px; border: 2px solid black;">Search</button>
+    <button @click="search" style="width: 100px; border: 2px solid black;">Search</button>
     <hr style="width: 90%;">
-
+    
     <div>
       <p>
-        <input type="checkbox" id="1" style="width: 20px;">1
-        <input type="checkbox" id="3" style="width: 20px;">3
-        <input type="checkbox" id="5" style="width: 20px;">5
-        <input type="checkbox" id="7" style="width: 20px;">7
-        <input type="checkbox" id="9" style="width: 20px;">9
-        <input type="checkbox" id="11" style="width: 18px;">11
-        <input type="checkbox" id="13" style="width: 18px;">13
-        <input type="checkbox" id="15" style="width: 18px;">15
-        <input type="checkbox" id="17" style="width: 18px;">17
-        <input type="checkbox" id="19" style="width: 18px;">19
-        <input type="checkbox" id="21" style="width: 18px;">21
-        <input type="checkbox" id="23" style="width: 18px;">23
-        <input type="checkbox" id="25" style="width: 18px;">25
-        <input type="checkbox" id="27" style="width: 18px;">27
+        <template v-for="i of 28" :key="i">
+          <input v-if="i%2!=0" type="checkbox" :disabled="tikets[direction+'/'+choiceDate]?.includes(i)" :value="i" v-model="seat" style="width: 20px;">
+          <span v-if="i%2!=0">{{ i }}</span>
+        </template>
       </p>
     </div>
     <div>
       <p>
-        <input type="checkbox" id="2" style="width: 20px;">2
-        <input type="checkbox" id="4" style="width: 20px;">4
-        <input type="checkbox" id="6" style="width: 20px;">6
-        <input type="checkbox" id="8" style="width: 20px;">8
-        <input type="checkbox" id="10" style="width: 18px;">10
-        <input type="checkbox" id="12" style="width: 18px;">12
-        <input type="checkbox" id="14" style="width: 18px;">14
-        <input type="checkbox" id="16" style="width: 18px;">16
-        <input type="checkbox" id="18" style="width: 18px;">18
-        <input type="checkbox" id="20" style="width: 18px;">20
-        <input type="checkbox" id="22" style="width: 18px;">22
-        <input type="checkbox" id="24" style="width: 18px;">24
-        <input type="checkbox" id="26" style="width: 18px;">26
-        <input type="checkbox" id="28" style="width: 18px;">28
+        <template v-for="i of 28" :key="i">
+          <input v-if="i%2==0" type="checkbox" :disabled="tikets[direction+'/'+choiceDate]?.includes(i)" :value="i" v-model="seat" style="width: 20px;">
+          <span v-if="i%2==0">{{ i }}</span>
+        </template>
       </p>
     </div>
     <table>
@@ -58,10 +38,11 @@
       <tr>
         <td>{{ direction }}</td>
         <td>{{ choiceDate }}</td>
-        <td>{{ seat }}</td>
+        <td>{{ seat.toString() }}</td>
       </tr>
     </table>
     <label for="trainDate">Total price: {{ cost }}</label>
+    <button @click="addTiket" style="width: 100px; border: 2px solid black;">Book</button>
   </div>
 
 </template>
@@ -71,27 +52,35 @@ import { ref } from 'vue'
 
 const direction = ref('')
 const choiceDate = ref('')
-const seat = ref('')
+const seat = ref([])
 const cost = ref('124$')
 
 
-const tiket = localStorage.textArr ? JSON.parse(localStorage.textArr) : [] as BuyTiket[]
-const arr = ref(tiket as BuyTiket[])
+const tikets = localStorage.tikets ? JSON.parse(localStorage.tikets) : {} as any
+const arr = ref(tikets as any)
 
 type BuyTiket = {
   seat: any,
+  direction: string,
+  choiceDate: number,
+}
+
+const search = () => {
+  
 }
 
 const addTiket = () => {
   if (!direction.value || !choiceDate.value) {
     alert('Не хватает данных для покупки')
   } else {
-    arr.value.push({
-      seat: seat.value
-    })
-    localStorage.textArr = JSON.stringify(arr.value)
+    if (!tikets[direction.value+'/'+choiceDate.value]) {
+      tikets[direction.value+'/'+choiceDate.value] = seat.value
+    } else {
+      tikets[direction.value+'/'+choiceDate.value] = [...tikets[direction.value+'/'+choiceDate.value], ...seat.value]
+    }
+    localStorage.tikets = JSON.stringify(arr.value)
   }
-  seat.value = ''
+  seat.value = []
 }
 </script>
 
