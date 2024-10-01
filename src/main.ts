e7533f914b9ef901dd02d96bf93c0fbbb7c3c4b6
Genyasochi <eunasedkina@gmail.com>
2024-10-01 21:14:07 +0300

    _

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import './style.css'
 import App from './App.vue'
 import HeaderComp from './components/HeaderComponent.vue'
 import {router} from './routes'
+import { createPinia } from 'pinia'
 
 // createApp(App).mount('#app')
 const app = createApp(App)
+const pinia = createPinia()
 app.use(router)
+app.use(pinia)
 app.mount('#app')
 app.component('HeaderComp', HeaderComp )
